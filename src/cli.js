579bd0d93498e962eac33d743b655401ddad5c6a
AvailableGrigory <grigoryfedorov@bk.ru>
2022-04-26 23:02:48 +0300

    add ESlint

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
-import readlineSync from "readline-sync";
+/* eslint-disable import/prefer-default-export */
+import readlineSync from 'readline-sync';
 
 export const askName = () => {
-  const name = readlineSync.question("May I have your name? ");
+  const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
